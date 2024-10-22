27b7e9a47a4f76ad96cad85a62b598759d8c3a51
Mohammad Najad <mjlr0909@gmail.com>
2024-10-21 23:57:17 -0400

    short desc

--- backend/db/seeders/20230926000111-demo-user.js
+++ backend/db/seeders/20230926000111-demo-user.js
@@ -48,7 +48,7 @@ module.exports = {
       {
         name: "levi sticker",
         description:
-          "Introducing the Levi Your Ride is Filthy, Clean it Up Sticker Elevate your vehicle's cleanliness with our no-nonsense Levi car sticker Crafted from durable vinyl material, this sticker is built to withstand the elements and adhere seamlessly to your car's surface. Whether you're a fan of Attack on Titan or simply appreciate a spotless ride, Levi's commanding presence will motivate you to maintain the utmost cleanliness.",
+          "Keep your ride clean with the Levi sticker. Crafted from durable vinyl, this no-nonsense car decal is built to last, perfect for Attack on Titan fans or anyone who values a spotless vehicle.",
         price: 10,
         count: 100,
         imageUrl: "https://i.postimg.cc/0jhxJtbQ/Levi-Blue.png",
@@ -56,7 +56,7 @@ module.exports = {
       {
         name: "makima sticker",
         description:
-          "Introducing our Makima Keep Your Eyes on the Road or Die Sticker!Stay focused and stay safe with our captivating Makima sticker! Crafted with durable vinyl, this sticker is perfect for your car, laptop, or water bottle. With its bold design and powerful message, Keep Your Eyes on the Road or Die, it serves as a constant reminder to prioritize road safety.But that's not all! Our sticker is weatherproof and waterproof, ensuring its longevity no matter where you choose to display it!",
+          "Stay focused with the Makima 'Keep Your Eyes on the Road or Die' sticker. Made from durable, weatherproof vinyl, it's perfect for cars, laptops, or water bottles, reminding you to prioritize safety.",
         price: 10,
         count: 100,
         imageUrl: "https://i.postimg.cc/VvX1nqMW/Makima-Wood.png",
@@ -64,7 +64,7 @@ module.exports = {
       {
         name: "Killua sticker",
         description:
-          "Introducing the Killua Wanna See Godspeed? Car Sticker!Unleash the racing beast within with our sleek Killua car sticker! Inspired by the lightning-fast agility of Godspeed, this sticker subtly poses the question: Wanna See GodspeedCrafted from durable vinyl designed to endure the speed and thrill of racing, this sticker adds a touch of adrenaline to your cars exterior!",
+          "Unleash your inner racer with the Killua 'Wanna See Godspeed?' car sticker. Made from durable vinyl, it's inspired by Killua's agility and adds a touch of adrenaline to your ride",
         price: 10,
         count: 100,
         imageUrl: "https://i.postimg.cc/pLHMb5jg/Killua-Pink.png",
@@ -72,7 +72,7 @@ module.exports = {
       {
         name: "Yake Energy",
         description:
-          "Introducing Yake Energy, the ultimate pick-me-up in a can! Bursting with exhilarating flavors and infused with a powerful blend of energizing ingredients, Yake Energy is your go-to companion for those moments when you need an extra boost of vitality. With its electrifying taste and invigorating fizz, each sip of Yake Energy sparks a wave of energy, propelling you through your day with unstoppable momentum. Whether you're conquering your workout, powering through a deadline, or seizing the day with gusto, Yake Energy is here to fuel your adventures and unleash your inner dynamo. Crack open a can and elevate your energy game to new heights with Yake Energy",
+          "Yake Energy is your ultimate pick-me-up, bursting with bold flavors and a powerful blend of energizing ingredients. Perfect for workouts, deadlines, or daily adventures, each sip fuels unstoppable momentum",
         price: 55,
         count: 100,
         imageUrl: "https://i.postimg.cc/qqTgNdcM/Designer.jpg",
@@ -80,7 +80,7 @@ module.exports = {
       {
         name: "Galactic Gush",
         description:
-          "Embark on a taste odyssey with Galactic Gush, the cosmic soda sensation that's out of this world! With each fizzy sip, you'll be transported on a flavor journey through the far reaches of the galaxy. Feel the rush of stardust-infused bubbles as they dance across your taste buds, delivering a burst of intergalactic flavors that defy earthly imagination. From the tangy tang of asteroid orange to the cosmic crush of lunar lemon-lime, every drop of Galactic Gush is a cosmic delight. So grab a can, brace yourself for warp speed, and get ready to experience flavor that's truly light years ahead!",
+          "Galactic Gush is a cosmic soda that takes your taste buds on an interstellar journey. Enjoy stardust-infused bubbles with flavors like asteroid orange and lunar lemon-lime. It's truly out of this world!",
         price: 40,
         count: 100,
         imageUrl: "https://i.postimg.cc/MTbqrfRq/Designer-1.jpg",
@@ -88,7 +88,7 @@ module.exports = {
       {
         name: "Dreamcatcher Delight Tea",
         description:
-          "Indulge in a moment of tranquility with Dreamcatcher Delight Tea, a soothing blend crafted to capture the essence of peaceful dreams and gentle serenity. Infused with the whispers of moonlit nights and the soft caress of gentle breezes, each sip of this enchanting tea envelops you in a cocoon of calm and tranquility. Let the delicate flavors of chamomile, lavender, and vanilla dance on your palate, soothing your senses and lulling you into a state of blissful relaxation. Whether enjoyed as a morning ritual or a nighttime indulgence, Dreamcatcher Delight Tea is your invitation to drift away on clouds of dreams and awaken refreshed, rejuvenated, and ready to embrace the day.",
+          "Dreamcatcher Delight Tea is a soothing blend of chamomile, lavender, and vanilla that captures the essence of tranquility. Each sip brings calm and relaxation, perfect for morning or nighttime indulgence.",
         price: 25,
         count: 100,
         imageUrl: "https://i.postimg.cc/j2S0ggt4/Designer-3.jpg",
@@ -96,7 +96,7 @@ module.exports = {
       {
         name: "Sparkleberry Fizz",
         description:
-          "Get ready to experience a burst of joy with Sparkleberry Fizz, the sparkling sensation that's as effervescent as it is delightful! Infused with the essence of ripe, juicy berries and a touch of sparkling magic, each sip of Sparkleberry Fizz is a celebration in a bottle. Feel the fizzy bubbles dance on your tongue, tickling your taste buds with bursts of fruity flavor and effervescent effervescence. With its vibrant hues and sparkling charm, Sparkleberry Fizz is the perfect companion for any occasion, whether you're toasting to a special moment or simply adding a bit of sparkle to your day. So pop the cap, raise your glass, and let the sparkle begin with Sparkleberry Fizz",
+          "Sparkleberry Fizz is a berry-infused sparkling drink that brings joy in every sip. With vibrant hues and fizzy bubbles, it's perfect for celebrations or adding a touch of sparkle to your day.",
         price: 30,
         count: 100,
         imageUrl: "https://i.postimg.cc/nrP5dndn/Designer-4.jpg",
@@ -104,7 +104,7 @@ module.exports = {
       {
         name: "Celestial Serenade Syrup",
         description:
-          "Embark on a celestial journey with Celestial Serenade Syrup, a magical elixir that transforms every moment into a celestial symphony of sweetness. Crafted under the watchful gaze of the stars and infused with the essence of cosmic harmony, each drop of this enchanting syrup is a melody for the senses",
+          "Celestial Serenade Syrup is a magical elixir that turns every moment into a sweet, celestial symphony. Infused with cosmic harmony, each drop is a melody for the senses",
         price: 70,
         count: 100,
         imageUrl: "https://i.postimg.cc/rF2YwZvn/Designer-6.jpg",
@@ -119,7 +119,7 @@ module.exports = {
       {
         name: "Aurora Shard",
         description:
-          "Unlock the secrets of the cosmos with the Oracle's Eye Amulet, a mystical artifact that holds the key to ancient wisdom and divine insight. Crafted by the hands of celestial artisans and imbued with the essence of cosmic magic, each Oracle's Eye Amulet is a conduit for divine guidance and mystical revelation. Wear it close to your heart and feel its power resonate within you, opening your eyes to the hidden truths that lie beyond the veil of reality. Whether you seek answers to life's mysteries or guidance on your spiritual journey, the Oracle's Eye Amulet is your faithful companion, guiding you with its all-seeing gaze and illuminating the path to enlightenment",
+          "The Oracle's Eye Amulet is a mystical artifact, crafted with cosmic magic to unlock ancient wisdom and divine insight. Wear it close to uncover hidden truths and guide your spiritual journey",
         price: 10,
         count: 100,
         imageUrl: "https://i.postimg.cc/xTWj8Q0r/Designer-8.jpg",
@@ -127,7 +127,7 @@ module.exports = {
       {
         name: "Soulstone Pendant",
         description:
-          "Unleash the power of the soul with the Soulstone Pendant, a mystical artifact adorned with celestial gemstones and ancient symbols. Said to contain the essence of the soul itself, this legendary pendant channels the energies of the cosmos, granting its wearer a connection to the realms beyond. Unlock the mysteries of existence and embark on a journey of spiritual enlightenment with the Soulstone Pendant as your guide.",
+          "The Soulstone Pendant, adorned with celestial gemstones, channels cosmic energy to connect its wearer to realms beyond. Unlock spiritual enlightenment with this mystical artifact",
         price: 25,
         count: 100,
         imageUrl: "https://i.postimg.cc/qRb4TVm5/Designer-9.jpg",
@@ -135,7 +135,7 @@ module.exports = {
       {
         name: "Dreamweaver's Locket",
         description:
-          "Embark on a journey through the realms of dreams with the Dreamweaver's Locket, a magical talisman that holds the essence of dreams within its gleaming frame. As you wear this enchanting locket close to your heart, feel the whispers of imagination and the shimmering threads of dreams intertwining with your reality. Let the Dreamweaver's Locket be your portal to a world of limitless possibilities, where dreams take flight and wishes come true",
+          "The Dreamweaver's Locket is a magical talisman that holds the essence of dreams. Wear it close to your heart and let it be your portal to limitless possibilities and wish fulfillment",
         price: 80,
         count: 100,
         imageUrl: "https://i.postimg.cc/htcDTr7M/Designer-11.jpg",
